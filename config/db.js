e7a4d61f79d73ebc84bf362271172d71b53b25cf
Entropic Cat <86585719+juan2284@users.noreply.github.com>
2023-09-27 01:39:54 -0400

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize';
 
-const db = new Sequelize('memorial', 'root', '', {
-  host: '127.0.0.1',
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
   port: '3306', 
   dialect: 'mysql',
   define: {
