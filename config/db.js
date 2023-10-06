1f75e41911104959c3f138f681215d3288586b1c
Juan <juanitogye@gmail.com>
2023-10-05 23:23:58 -0400

    Agregando Lazy Loading

--- config/db.js
+++ config/db.js
@@ -1,8 +1,5 @@
 import Sequelize from 'sequelize';
-<<<<<<< HEAD
 import dotenv from 'dotenv/config';
-=======
->>>>>>> e7a4d61f79d73ebc84bf362271172d71b53b25cf
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
@@ -20,8 +17,4 @@ const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.D
   operatorAliases: false
 });
 
-<<<<<<< HEAD
 export default db;
-=======
-export default db;
->>>>>>> e7a4d61f79d73ebc84bf362271172d71b53b25cf
